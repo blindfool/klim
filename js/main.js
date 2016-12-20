e37fbff5a4650fbd29ecd68eb2f9afc368e2e19e
Mikhail Zakharov <m.zakharov.mail@gmail.com>
2016-12-20 23:52:59 +0300

    klim initial

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/*MAIN JS*/
+
+/*Functions*/
+function CheckPageSize() {
+	$(window).resize(function() {
+		try{
+			var headerHeight = $('header').outerHeight(),
+				footerHeight = $('footer').outerHeight();
+				
+			$('.content-wrap').css('padding-top', headerHeight + 'px');	
+			$('footer').css('margin-top', '-' + footerHeight + 'px');
+			$('.all-wrap').css('margin-bottom', '-' + footerHeight + 'px');
+			$('.content-wrap').css('padding-bottom', footerHeight + 25 + 'px');
+			
+			if($('.slide-content')){
+				var scHeight = $('.slide-content').outerHeight();
+				$('.slide-content').css('margin-top', '-' + scHeight/2 + 'px');
+			}
+			
+		}
+		catch(e){}
+	});
+}
+
+/*Ready*/
+$(document).ready(function(){
+	
+	CheckPageSize();
+	setTimeout(function() {$(window).resize();}, 100);
+	
+	/*slick*/
+	$('.main-slider').slick({
+ 		autoplay: true,
+		autoplaySpeed: 5000,
+		adaptiveHeight: true,
+		arrows: true,
+		dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+	});
+	
+	/*filter*/
+	$(".field-toggle").click(function() {
+		var clickitem = $(this),
+			icon = clickitem.find('i');
+			
+/* 		if(icon.hasClass("fa-angle-down")) {
+			icon.removeClass("fa-angle-down").addClass("fa-angle-up");
+		} else {
+			icon.removeClass("fa-angle-up").addClass("fa-angle-down");
+		} */
+		if(icon.hasClass("collapsed")) {
+			icon.removeClass("collapsed");
+		} else {
+			icon.addClass("collapsed");
+		} 		
+		clickitem.parents('.field').find(".field-body").slideToggle();
+	});	
+	
+});
+
+/*Events*/
+$(window).scroll(function(){
+	
+});
+
+$(window).resize(function(){
+
+});
